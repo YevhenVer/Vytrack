d7e9a513721467d14e7beef279ae60daee1d6ec2
hilolabur <khilola.b5@gmail.com>
2025-02-27 14:12:25 -0500

    Revert "Marketing page filter is done!"
    This reverts commit a4595067db5ddf5eaad176765a2fa1e231d61798.

--- src/test/java/com/vytrack/step_definitions/PinbarStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/PinbarStepDefs.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
-public class PinbarUsingStepDefs {
+public class PinbarStepDefs {
 
     PinbarPage pinbarPage = new PinbarPage();
 
